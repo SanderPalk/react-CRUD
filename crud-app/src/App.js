cd008a52616163e4533eea75104f2d33cf499083
SanderPalk <95194970+SanderPalk@users.noreply.github.com>
2022-11-15 16:03:13 +0200

    Done [Delivered #183804795]

--- crud-app/src/App.js
+++ crud-app/src/App.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
+import {AddPicture} from "./components/AddPicture";
 import {Picture} from "./components/Home";
 
 export default function App() {
     const [photos, setPhotos] = useState([]);
 
-    const api = "https://jsonplaceholder.typicode.com/photos"
+    const api = "https://jsonplaceholder.typicode.com/photos/"
 
     useEffect(() => {
         fetchData();
@@ -17,9 +18,86 @@ export default function App() {
             .catch((error) => console.log(error));
     };
 
+    const onAdd = async (title, url) => {
+        await fetch( api, {
+            method: "POST",
+            body: JSON.stringify({
+                title: title,
+                url: url,
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then((response) => {
+                if (response.status !== 201) {
+                    return;
+                } else {
+                    return response.json();
+                }
+            })
+            .then((data) => {
+                setPhotos((comments) => [...comments, data]);
+            })
+            .catch((error) => console.log(error));
+    };
+
+    const onEdit = async (id, title, url) => {
+        await fetch(api + {id}, {
+            method: "PUT",
+            body: JSON.stringify({
+                title: title,
+                url: url,
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then((response) => {
+                if (response.status !== 200) {
+                    return;
+                } else {
+                    return response.json();
+                }
+            })
+            .then((data) => {
+                // setUsers((users) => [...users, data]);
+                const updatedPhotos = photos.map((photo) => {
+                    if (photo.id === id) {
+                        photo.title = title;
+                        photo.url = url;
+                    }
+
+                    return photo;
+                });
+
+                setPhotos((photos) => updatedPhotos);
+            })
+            .catch((error) => console.log(error));
+    };
+
+    const onDelete = async (id) => {
+        await fetch(api + {id}, {
+            method: "DELETE"
+        })
+            .then((response) => {
+                if (response.status !== 200) {
+                    return;
+                } else {
+                    setPhotos(
+                        photos.filter((photo) => {
+                            return photo.id !== id;
+                        })
+                    );
+                }
+            })
+            .catch((error) => console.log(error));
+    };
+
     return (
         <div className="App">
             <h1>Photos</h1>
+            <AddPicture onAdd={onAdd}/>
             <div className="form_body">
                 {photos.map((photo) => (
                     <Picture
@@ -27,8 +105,8 @@ export default function App() {
                         key={photo.id}
                         title={photo.title}
                         url={photo.url}
-                        onEdit={"onEdit"}
-                        onDelete={"onDelete"}
+                        onEdit={onEdit}
+                        onDelete={onDelete}
                     />
                 ))}
             </div>
--- crud-app/src/components/Home.js
+++ crud-app/src/components/Home.js
@@ -1,12 +1,38 @@
 import React, { useState } from "react";
 
-export const Picture = ({ title, url}) => {
+export const Picture = ({ title, url, id, onEdit, onDelete }) => {
+    const [isEdit, setIsEdit] = useState(false);
+
+    const handleOnEditSubmit = (evt) => {
+        evt.preventDefault();
+        onEdit(id, evt.target.title.value, evt.target.url.value);
+        setIsEdit(!isEdit);
+    };
+
+    const handleEdit = () => {
+        setIsEdit(!isEdit);
+    };
+
+    const handleDelete = () => {
+        onDelete(id);
+    };
+
     return (
         <div>
-            <div className="photo">
-                <span className="title">{title}</span>
-                <img src={url} width={"10%"} height={"10%"}/>
-            </div>
+            {isEdit ? (
+                <form onSubmit={handleOnEditSubmit}>
+                    <input placeholder="Title" name="title" defaultValue={title} />
+                    <input placeholder="Url" name="url" defaultValue={url} />
+                    <button onSubmit={handleOnEditSubmit}>Save</button>
+                </form>
+            ) : (
+                <div className="photo">
+                    <span className="title">{title}</span>
+                    <img src={url} width={"10%"} height={"10%"}/>
+                    <button onClick={handleEdit}>Edit</button>
+                    <button onClick={handleDelete}>Delete</button>
+                </div>
+            )}
         </div>
     );
 };
